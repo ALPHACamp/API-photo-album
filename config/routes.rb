b6f01e5bf6ff8fc9cb25e526342a6e1043fc1d88
pochia <lupochia@gmail.com>
2018-01-01 21:35:54 +0800

    add api user and photo route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :photos
+
+
+  namespace :api, defaults:  { format:  :json } do
+    resources :users, only: [:index, :show, :update, :delete]
+    resources :photos, only: [:index, :show, :update, :delete]
+  end
+
+
+
 end
