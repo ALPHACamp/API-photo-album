5084c6f546dfbb221c8699125d6ab079563cde5f
pochia <lupochia@gmail.com>
2018-01-03 22:14:09 +0800

    add api controller photo index

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -2,8 +2,12 @@ class Api::PhotosController < Api::ApplicationController
 
 
   def index
-    
+    @photos = Photo.all
+    respond_to do |format|
+      format.json { render json: @photos }
+    end
   end
+
 
 
 
