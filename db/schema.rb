b44172de9d1c814a31b573045622103e68b448b8
pochia <lupochia@gmail.com>
2018-01-08 18:03:37 +0800

    add migration token to user and add user model to generate token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101085744) do
+ActiveRecord::Schema.define(version: 20180108100121) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20180101085744) do
     t.integer "age"
     t.string "phone"
     t.string "photo"
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
